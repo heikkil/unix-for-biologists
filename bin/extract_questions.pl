6877f090e629e986aeefa6c488467bc46200e8d7
Heikki Lehvaslaiho <Heikki.Lehvaslaiho@kaust.edu.sa>
2011-10-30 08:42:01 +0300

    simplify the question generation

--- bin/extract_questions.pl
+++ bin/extract_questions.pl
@@ -2,20 +2,17 @@
 use Modern::Perl;
 use autodie;
 
+# open input file
 my $file = shift;
 $file ||= 'questions_with_answers.org';
-
 open my $F, "<", $file;
 
 our $OUT;    # output files
-
 my $question = 0;
-
 
 while (<$F>) {
     # sessions
-    #say if /^\* /;
-
+    # open a new file for each section
     if (/^\* /) {
 	my $line = $_;
 	chomp;
@@ -24,13 +21,16 @@ while (<$F>) {
 	my $outfile = $_. ".txt"; say $outfile;
 	open $OUT, ">", $outfile;
 	say $OUT $line;
+	next;
     }
 
-
+    # what defines the range of lines in questions
     $question = 1 if /^\*\*/;
     $question = 0 if /\\pause/;
 
-    next if /^\*\*/;
+    # every frame needs to be numbered
+    print $_  if /^\*\*/ and not /^\*\* *(\d+\.)/; # debug
+    print $OUT "$1 " and next if /^\*\* *(\d+\.)/;
     next if /^\#/;
     s/\- +//;
     s/\\texttt\{([^}]+)\}/$1/g;
