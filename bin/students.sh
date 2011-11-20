0423c34a2e153bdfee4b41f50b608d852956d613
Heikki Lehvaslaiho <Heikki.Lehvaslaiho@kaust.edu.sa>
2011-11-20 07:58:44 +0300

    print out info only if identity file exists

--- bin/students.sh
+++ bin/students.sh
@@ -1,14 +1,16 @@
 #!/bin/bash 
 
-# When run on the remote computer with student accounts, this script
-# creates a listing of all students from a file ~/identity:
+# when run on the remote computer with student accounts, this script
+# creates a listing of all students from a file ~/name:
 #
 # full name
 # KAUST ID
 #
 
 for LOGIN in $(ls /home/ | grep student ); do
-    echo \#---------------
-    echo $LOGIN
-    cat /home/$LOGIN/identity
+    if [ -e /home/$LOGIN/identity ]; then
+	echo \#---------------
+	echo $LOGIN
+	cat /home/$LOGIN/identity
+    fi
 done
